54c7b7db5d927fd2f1ec42691ce171bd6e37bfc8
Moe-Hassan123 <mohamedalfidil123@gmail.com>
2020-04-14 14:25:16 -0700

    Moh-Update

--- ams-theater-database/controller/newMovieController.js
+++ ams-theater-database/controller/newMovieController.js
@@ -10,10 +10,10 @@ exports.addNewMovie = (req, res, next) => {
     movie_type: req.body.movie_type,
     company: req.body.company,
     director: req.body.director,
-    script_writer:req.body.script_writer,
+    script_writer: req.body.script_writer,
     main_star: req.body.main_star,
     co_star: req.body.co_star,
-    description: req.body.description
+    description: req.body.description,
   });
 
   newMovie
@@ -27,3 +27,27 @@ exports.addNewMovie = (req, res, next) => {
     });
 };
 
+exports.getMovies = (req, res, next) => {
+  MoviesSchema.find()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).send("couldn't get movies");
+    });
+};
+
+exports.detailMovie = (req, res, next) => {
+  console.log(req.params.id);
+
+  // res.send(200);
+    MoviesSchema.findById(req.params.id)
+      .then((result) => {
+        console.log(result);
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(400).send("couldn't get movies");
+      });
+};
